c06221d38a1439b5bf0b63a8cb50f4cbb471c2b6
micheleboanno92 <michelebonanno92@gmail.com>
2024-09-12 15:53:58 +0200

    header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+  data() {
+    return { 
+    
+    }
+  }
+}
+</script>
+
+<template>
+  <header>
+   <div class="container d-flex align-items-center">
+    <img src="/img/1.jpg" alt="logo">
+    <h1>
+      Yo-Gi-Oh Api
+    </h1>
+   </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+@use '../assets/scss/partials/variables' as *;
+
+header {
+  background-color: $headerBgColor;
+  text-align: center;
+  padding: 20px 0;
+  div > img {
+    height: 20px ;
+    margin-right: 10px;
+  }
+}
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,18 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// Import all of Bootstrap's JS
+import * as bootstrap from 'bootstrap'
+
+createApp(App).mount('#app')
+
+/*
+Descrizione:
+Create un nuovo progetto utilizzando Vite e Vue 3 e definite i componenti necessari per strutturare il layout come da screenshot allegato.
+Al caricamento della pagina, effettuate una chiama ajax all'API di Yu Gi Oh:
+e con i dati restituiti, stampate una card per ogni carta.
+ATTENZIONE: l'api restituisce tutti i risultati in un colpo solo. Per evitare attese e/o rallentamenti nelle richieste, potete diminuire il numero di risultati sfruttando i parametri num e offset
+https://db.ygoprodeck.com/api/v7/cardinfo.php?num=20&offset=0
+Bonus:
+Creare un componente loader da visualizzare fintantoché i risultati non sono pronti.
+*/
